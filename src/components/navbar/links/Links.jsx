1a496471e97891ac2d894b47cad70efc665348ba
sina saeedifar <sina.saeedifar@gmail.com>
2024-03-26 11:29:48 +0330

    change Links.jsx and change package.json file

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -24,25 +24,26 @@ const Links = () => {
     //TEMPORARY
     const isAdmin = true;
     const session = true;
-    return <div className={styles.container}>
-        <div className={styles.links}>
-            {links.map((link => <NavLink item={link} key={link.title}/>))}
-            {session ? (
-                <>
-                    {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}}/>}
-                    <button className={styles.logout}>Logout</button>
-                </>) : (
-                <NavLink item={{title: "Login", path: "/login"}}/>
-            )}
+    return (
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link => <NavLink item={link} key={link.title}/>))}
+                {session ? (
+                    <>
+                        {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}}/>}
+                        <button className={styles.logout}>Logout</button>
+                    </>) : (
+                    <NavLink item={{title: "Login", path: "/login"}}/>
+                )}
+            </div>
+            <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
+            {open && (<div className={styles.mobileLinks}>
+                {links.map((link) => {
+                    return <NavLink item={link} key={link.title} />
+                })}
+            </div>)}
         </div>
-        <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
-        {open && (<div className={styles.mobileLinks}>
-            {links.map((link) => {
-                <NavLink item={link} key={link.title} } />
-            ))
-            }
+    )
+}
 
-                </div>)}
-        </div>)
-        }
 export default Links;
