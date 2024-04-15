adc1dc7aa0b6faea1c1166e99d88f502fed268c7
sina saeedifar <sina.saeedifar@gmail.com>
2024-04-15 16:30:14 +0330

    adding card styles

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 const PostCard = () => {
     return <div className={styles.container}>
         <div className={styles.top}>
-            <Image className={styles.img} src={"https://plus.unsplash.com/premium_photo-1676823552681-bf1d1135fa2f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt={""} fill/>
+            <div className={styles.imgContainer}>
+            <Image className={styles.img} src={"/hero.png"} alt={""} fill/>
+            </div>
             <span className={styles.date}>01.01.2025</span>
         </div>
         <div className={styles.bottom}>
